9720a5e91492895e95dfe8406d4b15c3ddd1402c
Chris Trotman <chris@trotman.io>
2014-07-20 17:26:01 +1000

    [zsh] Fix up the man page directories for git-extras

--- .zsh/20-git-extras.zsh
+++ .zsh/20-git-extras.zsh
@@ -5,4 +5,8 @@ fi
 if [[ -e $HOME/.git-extras ]]; then
     export PATH="$PATH:$HOME/.git-extras/bin"
     antigen bundle git-extras
+
+    if [[ ! -e $HOME/.git-extras/man/man1 ]]; then
+        ln -s $HOME/.git-extras/man $HOME/.git-extras/man/man1
+    fi
 fi
