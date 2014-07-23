8e50c56a32293303ed1fc5e7a3c7aa2c0a0c3341
Chris Trotman <chris@trotman.io>
2014-07-23 19:40:00 +1000

    [zsh] Remove git-extras oh-my-zsh plugin

--- .zsh/20-git-extras.zsh
+++ .zsh/20-git-extras.zsh
@@ -4,7 +4,6 @@ fi
 
 if [[ -e $HOME/.git-extras ]]; then
     export PATH="$PATH:$HOME/.git-extras/bin"
-    antigen bundle git-extras
 
     if [[ ! -e $HOME/.git-extras/man/man1 ]]; then
         ln -s $HOME/.git-extras/man $HOME/.git-extras/man/man1
