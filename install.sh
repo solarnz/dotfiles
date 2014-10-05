1a5f529e5c5bc2f93bd45668fa64fb4c3323691f
Chris Trotman <chris@trotman.io>
2014-10-05 18:20:35 +1100

    [rcm] Use $HOME as the prefix to mktemp
    This fixes issues where /tmp is mounted noexec.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install rcm to $HOME/.local/
-TEMP=`mktemp -d`
+TEMP=`mktemp -d -p $HOME`
 pushd $TEMP
 wget https://thoughtbot.github.com/rcm/dist/rcm-1.2.3.tar.gz
 tar xvzf rcm-1.2.3.tar.gz
