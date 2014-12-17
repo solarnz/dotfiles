6e5c3e090a38c4940639e43039903b9ea7a7edeb
Chris Trotman <chris@trotman.io>
2014-12-17 22:17:47 +1100

    [aws_env] Rename alias so it doesn't conflict with the aws cli tools

--- zsh/20-aws.zsh
+++ zsh/20-aws.zsh
@@ -1,10 +1,10 @@
 export AWSPATH=$HOME/.aws
 
-function aws {
+function aws_env {
     source $AWSPATH/$1
 }
 
 function _aws_envs {
     reply=($(ls $AWSPATH))
 }
-compctl -K _aws_envs aws
+compctl -K _aws_envs aws_env
