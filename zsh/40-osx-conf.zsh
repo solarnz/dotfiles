ec66cbe356e08da733cf5c76774cbbe2f8f5f447
Chris Trotman <chris@trotman.io>
2016-12-25 20:32:25 -0800

    [zsh] Only use the gnu find/tar if they're available

--- zsh/40-osx-conf.zsh
+++ zsh/40-osx-conf.zsh
@@ -1,10 +1,15 @@
 if [[ $(uname) == "Darwin" ]]; then
     PATH="/usr/local/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/sbin:/usr/local/share/python/:/usr/local/share/npm/bin/:$PATH"
 
-    alias find="gfind"
-    alias tar="gtar"
+    if [[ -x "$(which gfind)" ]]; then
+        alias find="gfind"
+    fi
 
-    if [[ -x /usr/local/bin/gls ]]; then
+    if [[ -x "$(which gtar)" ]]; then
+        alias tar="gtar"
+    fi
+
+    if [[ -x "$(which gls)" ]]; then
         alias ls="gls --color=auto"
     fi
 fi
