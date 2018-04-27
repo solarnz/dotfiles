5606b476678f7d49348dbc1e7004eed2fae879f8
Chris Trotman <chris@trotman.io>
2018-04-27 20:03:15 +1000

    [zsh] Remove virtualenvwrapper support

--- zsh/10-virtualenvwrapper.zsh
+++
@@ -1,9 +0,0 @@
-export VIRTUALENVWRAPPER_PYTHON=`which python2 || which python`
-export WORKON_HOME=$HOME/virtual_envs
-
-if [[ ! -e "$WORKON_HOME" ]]; then
-    $VIRTUALENVWRAPPER_PYTHON -m pip install --user virtualenvwrapper
-    mkdir -p $WORKON_HOME
-fi
-
-source `which virtualenvwrapper.sh`
