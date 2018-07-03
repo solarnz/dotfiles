b45453a3e7182d0de22b176ef93573117fcbb464
Chris Trotman <chris@freelancer.com>
2018-07-03 21:39:04 +1000

    Use gpg-agent as the ssh agent

--- zsh/10-ssh-keys.zsh
+++ zsh/10-ssh-keys.zsh
@@ -1,5 +1,6 @@
 if [[ $SSH_CONNECTION == "" ]]; then
-    eval `keychain --eval keychain $(ls ~/.ssh | grep -v -e pub -e authorized_keys -e known_hosts -e config) `
+    eval `keychain --eval --agents gpg`
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
 
 # vim:ft=zsh
