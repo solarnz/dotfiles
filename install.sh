b2cdd041dd977f459c1499cd4d74d3e8787a10c2
Chris Trotman <chris@trotman.io>
2021-11-27 14:46:15 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Install rcm to $HOME/.local/
-TEMP=`mktemp -d -p $HOME`
+mkdir /tmp/dotfiles
+TEMP=/tmp/dotfiles
 pushd $TEMP
-wget https://thoughtbot.github.com/rcm/dist/rcm-1.2.3.tar.gz
-tar xvzf rcm-1.2.3.tar.gz
+wget https://github.com/thoughtbot/rcm/archive/refs/tags/v1.2.3.tar.gz
+tar xvzf v1.2.3.tar.gz
 pushd rcm-1.2.3
+./autogen.sh
 ./configure --prefix=$HOME/.local/
 make && make install
 
 popd
 popd
-rm -rf $TEMP
+#rm -rf $TEMP
 
 # Install dotfiles
 PATH=$PATH:$HOME/.local/bin $HOME/.local/bin/rcup -d `dirname $0`
