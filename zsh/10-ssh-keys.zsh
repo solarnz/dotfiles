f49a4e89c803fb37e4e38a1b6db39a3e9088ee5c
Chris Trotman <chris@trotman.io>
2021-11-27 14:52:53 +1100

    zsh: Add Darwin customisations

--- zsh/10-ssh-keys.zsh
+++ zsh/10-ssh-keys.zsh
@@ -1,6 +1,12 @@
-if [[ $SSH_CONNECTION == "" ]]; then
-    eval `keychain --eval --agents gpg`
-    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+if [[ $(uname) == "Darwin" ]]; then
+    if [[ $SSH_CONNECTION == "" ]]; then
+        eval `keychain --eval --agents ssh`
+    fi
+else
+    if [[ $SSH_CONNECTION == "" ]]; then
+        eval `keychain --eval --agents gpg`
+        export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+    fi
 fi
 
 # vim:ft=zsh
--- zsh/30-aliases.zsh
+++ zsh/30-aliases.zsh
@@ -3,7 +3,12 @@ alias ll="ls -la"
 alias l="ls -l"
 alias sl="ls"
 
-alias ls="ls --color"
+if [[ $(uname) == "Darwin" ]]; then
+    alias ls="ls -G"
+else
+    alias ls="ls --color"
+fi
+
 
 alias fgc="git fzf-git-checkout"
 alias fdb="git delete-branches"
